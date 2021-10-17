6a4a1f4fd263ae14943249f6c1df845cfb64a011
Robin Vandamme <robin.vandamme@komposite.fr>
2021-10-17 11:45:04 +0200

    [ADD] Basic main function

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+def main():
+    nb_clients = input("Number of clients : ")
+    nb_servers = input("Number of servers : ")
+    print("There is " + nb_clients + " clients and " + nb_servers + " servers")
+
+if __name__ == "__main__":
+    main()
