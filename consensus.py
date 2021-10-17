3ad16953fc9efeb8503e3800f5a50e2a233ba34a
Robin Vandamme <robin.vandamme@komposite.fr>
2021-10-17 16:16:10 +0200

    [ADD] init clients and servers

--- consensus.py
+++ consensus.py
@@ -1,15 +1,53 @@
 from mpi4py import MPI
+import sys
+
+nb_clients = int(sys.argv[1])
+nb_servers = int(sys.argv[2])
+term = 0
+
+def clients():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    print(str(rank) + " : I'm a client")
+    for i in range(nb_servers):
+        req = comm.isend(rank, dest=i)
+        req.wait()
+
+def init_servers():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    data = [None] * nb_clients
+    for i in range(nb_clients):
+        req = comm.irecv(source=nb_servers + i)
+        data[i] = req.wait()
+    print("server nb " + str(rank) + " ")
+    print(data)
+    return data
 
 def consensus():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    print(str(rank) + " time for consensus")
+    current_term = term
+    return rank
 
-    if rank == 0:
-        data = {'a': 7, 'b': 3.14}
-        comm.send(data, dest=1, tag=11)
-    elif rank == 1:
-        data = comm.recv(source=0, tag=11)
-        print("1 received from 0 : ")
-        print(data)
+def servers():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    data = init_servers()
+    leader = consensus(data)
+    print("leader is " + leader + ", responding to " + data[0])
+    elt = data[0]
+    data.remove(elt)
 
-consensus()
+def main():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    
+    if (rank < nb_servers):
+        servers()
+    if (rank >= nb_servers and rank < (nb_clients + nb_servers)):
+        clients()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def main():
     nb_clients = input("Number of clients : ")
     nb_servers = input("Number of servers : ")
     print("There is " + nb_clients + " clients and " + nb_servers + " servers")
+    nb_process = int(nb_servers) + int(nb_clients)
+    s = "mpiexec -n " + str(nb_process) + " --mca opal_warn_on_missing_libcuda 0 --oversubscribe python consensus.py " + nb_clients + " " + nb_servers
+    print(s)
+    os.system(s)
 
 if __name__ == "__main__":
     main()
