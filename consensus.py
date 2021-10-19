aef8916fbea703d74758078af1d304d922155d26
Robin Vandamme <robin.vandamme@komposite.fr>
2021-10-19 18:33:56 +0200

    [ADD] added leader role

--- consensus.py
+++ consensus.py
@@ -18,18 +18,15 @@ class Server():
         self.rank = rank
         self.role = "FOLLOWER"
         self.term = 0
-
-    def recvFromClients(self):
-        data = [None] * nb_clients
+        self.data = [None] * nb_clients
         for i in range(nb_clients):
             req = comm.irecv(source=nb_servers + i)
-            data[i] = req.wait()
-        return data
+            self.data[i] = req.wait()
 
     def argmax(iterable):
         return max(enumerate(iterable), key=lambda x: x[1])[0]
 
-    def handleMessage(self, term):
+    def handleMessage(self):
 
         status = MPI.Status()
         is_message = comm.Iprobe(status=status)
@@ -48,44 +45,30 @@ class Server():
         self.term += 1
 
         vote = [0] * nb_servers
-        voted = False
-
-        # Vote for himself
-        self.vote[self.rank] += 1
-        voted = True
-
 
         if self.role == "CANDIDATE":
             for i in range(nb_servers):
                 if i == self.rank:
                     continue
-                req = comm.isend(term, dest=i)
+                req = comm.isend(self.term, dest=i)
                 req.wait()
 
 
         current_time = time.time()
         timeout = current_time + time.struct_time(tm_sec=5)
 
-        while True:
-            if self.role == "FOLLOWER":
+        while (current_time <= timeout):
+            if self.role != "LEADER":
                 current_time = time.time()
-            # Too long
-            if (current_time > timeout):
-                self.role = "CANDIDATE"
-                return self.consensus()
+            self.handleMessage()
 
-            self.handleMessage(term)
+        # Too long
+        self.role = "CANDIDATE"
 
-        for i in range(nb_servers):
-            if i == self.rank:
-                continue
-            req = comm.irecv(source=i)
-            print("server " + str(self.rank) + " waiting for response")
-            if req.get_status():
-                vote[self.rank] += req.wait()
-            else:
-                req.cancel()
+        # Vote for himself
+        self.vote[self.rank] += 1
 
+        return self.consensus()
 
     def run(self):
         data = init_servers()
@@ -97,72 +80,7 @@ class Server():
             data.remove(elt)
             print(data)
             time.sleep(1)
-"""
-    def consensus(self, term):
-        term += 1
-        print(f"Server {self.rank} start consensus")
-        vote = [0] * nb_servers
-        voted = False
 
-        for i in range(nb_servers):
-            if i == self.rank:
-                continue
-            req = comm.irecv(source=i)
-            if req.get_status():
-                print(str(self.rank) + " received vote from " + str(i))
-                recv_term = req.wait()
-                if recv_term < term:
-                    continue
-                term = recv_term
-                vote[i] += 1 + vote[self.rank]
-                vote[self.rank] = 0
-                voted = True
-                req.cancel()
-                req = comm.isend(vote[i], dest=i, tag=1)
-                req.wait()
-
-        if voted:
-            leader = argmax(vote)
-            req = comm.irecv(source=leader, tag=2)
-            print("server number " + str(self.rank) + " waiting for leader " + str(leader))
-            term = req.wait()
-            return term, leader
-
-        vote[self.rank] += 1
-        voted = True
-
-        for i in range(nb_servers):
-            req = comm.isend(term, dest=i, tag=0)
-            req.wait()
-
-        for i in range(nb_servers):
-            if i == self.rank:
-                continue
-            req = comm.irecv(source=i)
-            print("server " + str(self.rank) + " waiting for response")
-            if req.get_status():
-                vote[self.rank] += req.wait()
-            else:
-                req.cancel()
-
-        print("server number " + str(self.rank))
-        print(vote)
-
-        leader = argmax(vote)
-        if vote[leader] < nb_servers // 2 + 1:
-            print("server number " + str(self.rank) + " does not have enough vote")
-            return consensus(term)
-
-        for i in range(nb_servers):
-            if i == self.rank:
-                continue
-            print("sent to " + str(i))
-            req = comm.isend(term, dest=i, tag=2)
-            req.wait()
-        print("server number " + str(self.rank) + " is sleeping")
-
-        return (term, leader)
-"""
 class Client():
     """docstring for Client."""
 
@@ -185,7 +103,6 @@ def main():
     if (rank >= nb_servers and rank < (nb_clients + nb_servers)):
         myClient = Client(rank)
         myClient.run()
-
 
 
 
