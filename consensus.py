f85bd171f66693b2f2a85ff5870974d5e313589b
Robin Vandamme <robin.vandamme@komposite.fr>
2021-10-19 20:13:19 +0200

    [ADD] handle recv, handle send and handle log for handling all the communications between servers

--- consensus.py
+++ consensus.py
@@ -7,6 +7,9 @@ import time
 nb_clients = int(sys.argv[1])
 nb_servers = int(sys.argv[2])
 comm = MPI.COMM_WORLD
+majority = nb_servers // 2 + 1
+
+VOTE_REQ, VOTE_POS, VOTE_NEG, HEARTBEAT = 0,1,2,3
 
 
 class Server():
@@ -18,21 +21,91 @@ class Server():
         self.rank = rank
         self.role = "FOLLOWER"
         self.term = 0
+        self.timeout = 0
+        self.leader_heartbeat = 0
+        self.request_vote = 0
+        self.log = []
+        self.vote = [-1] * nb_servers
         self.data = [None] * nb_clients
         for i in range(nb_clients):
             req = comm.irecv(source=nb_servers + i)
             self.data[i] = req.wait()
 
-    def argmax(iterable):
-        return max(enumerate(iterable), key=lambda x: x[1])[0]
 
-    def handleMessage(self):
+    def respond(self):
+        comm.isend(self.data[-1], dest=self.data[-1])
+        return self.data.pop()
+
+
+    def handleLog(self, log):
+        for i in range(len(self.log), len(log)):
+            self.log.append(log[i])
+            self.data.remove(log[i])
+
+
+    def handleRecv(self):
 
         status = MPI.Status()
         is_message = comm.Iprobe(status=status)
-        if (is_message):
-            src = status.source #status.Get_source()
-            msg = comm.irecv()
+
+        if (not is_message):
+            return
+
+        src = status.source #status.Get_source()
+        tag = status.tag
+        term = comm.irecv().wait()
+        print("server number " + str(self.rank) + " source : " + str(src) + " tag : " + str(tag) + " term : " + str(term))
+
+        if (tag == VOTE_REQ):
+            if term > self.term:
+                self.term = term
+                self.vote = [-1] * nb_servers
+                self.vote[self.rank] = src
+                self.role = "FOLLOWER"
+                self.timeout += 2
+                comm.isend(self.term, dest=src, tag=VOTE_POS)
+            else:
+                comm.isend(self.term, dest=src, tag=VOTE_NEG)
+
+        if (tag == VOTE_POS):
+            self.vote[src] = self.rank
+            nb_vote = len([vote for vote in self.vote if vote == self.rank])
+            if nb_vote >= majority:
+                print("server number " + str(self.rank) + " is now leader")
+                self.role = "LEADER"
+                self.leader_heartbeat = time.time()
+
+        if (tag == VOTE_NEG):
+            self.vote[src] = -2
+
+        if (tag == HEARTBEAT):
+            self.timeout += 2
+            term, log = term
+            self.term = term
+            self.handleLog(log)
+
+
+    def handleSend(self):
+
+        tmp = time.time()
+        if self.role == "CANDIDATE":
+            if (tmp > self.request_vote + 1):
+                print("server number " + str(self.rank) + " is sending VOTE_REQ to everyone")
+                self.request_vote = time.time()
+                for i in range(nb_servers):
+                    if self.vote[i] == -1:
+                        req = comm.isend(self.term, dest=i, tag=VOTE_REQ)
+
+        if self.role == "LEADER":
+            if len(self.data) > 0:
+                value = self.respond()
+                self.log.append(value)
+            if (tmp > self.leader_heartbeat + 1):
+                self.leader_heartbeat = time.time()
+                for i in range(nb_servers):
+                    if i != self.rank:
+                        comm.isend((self.term, self.log), dest=i, tag=HEARTBEAT)
+
 
     def consensus(self):
         #follower
@@ -42,43 +115,37 @@ class Server():
         votez pour moi
         infini et si leader il reset son timeout jusqu'à avoir plus de la moitié
         """
+        terminate = False
         self.term += 1
 
-        vote = [0] * nb_servers
-
-        if self.role == "CANDIDATE":
-            for i in range(nb_servers):
-                if i == self.rank:
-                    continue
-                req = comm.isend(self.term, dest=i)
-                req.wait()
-
         current_time = time.time()
-        timeout = current_time + time.struct_time(tm_sec=5)
+        self.timeout = current_time + random.randint(3,5)
 
-        while (current_time <= timeout):
+        while (current_time <= self.timeout) and not terminate:
             if self.role != "LEADER":
                 current_time = time.time()
-            self.handleMessage()
+            self.handleRecv()
+            self.handleSend()
+            if len(self.data) == 0:
+                time.sleep(1)
+                self.handleSend()
+                terminate = True
 
         # Too long
+        print("server number " + str(self.rank) + " is now candidate")
         self.role = "CANDIDATE"
 
         # Vote for himself
-        self.vote[self.rank] += 1
+        self.vote[self.rank] = self.rank
 
-        return self.consensus()
+        return terminate
 
     def run(self):
-        data = init_servers()
-        term = 0
-        while len(data) > 0:
-            term, leader = self.consensus(term)
-            print("server number " + str(self.rank) + ", leader is " + str(leader) + ", responding to " + str(data[0]))
-            elt = data[0]
-            data.remove(elt)
-            print(data)
-            time.sleep(1)
+        terminate = False
+        while not terminate:
+            terminate = self.consensus()
+        print("server number " + str(self.rank) + " log : " + str(self.log))
+
 
 class Client():
     """docstring for Client."""
@@ -91,13 +158,13 @@ class Client():
         print(str(self.rank) + " : I'm a client")
         for i in range(nb_servers):
             req = comm.isend(self.rank, dest=i)
-            req.wait()
 
 def main():
     rank = comm.Get_rank()
 
     if (rank < nb_servers):
         myServer = Server(rank)
+        myServer.run()
 
     if (rank >= nb_servers and rank < (nb_clients + nb_servers)):
         myClient = Client(rank)
