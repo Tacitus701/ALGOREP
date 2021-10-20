4eadd1dae67972dc288d7403f04513ae919fd112
ajix <alexandre.pedrosa@epita.fr>
2021-10-20 13:43:05 +0200

    [disk] updating term now saves term value to disk

--- consensus.py
+++ consensus.py
@@ -1,19 +1,20 @@
 from mpi4py import MPI
-from enum import Enum
 import sys
 import random
 import time
+import os
 
 nb_clients = int(sys.argv[1])
 nb_servers = int(sys.argv[2])
 comm = MPI.COMM_WORLD
 majority = nb_servers // 2 + 1
 
-VOTE_REQ, VOTE_POS, VOTE_NEG, HEARTBEAT = 0,1,2,3
+VOTE_REQ, VOTE_POS, VOTE_NEG, HEARTBEAT = 0, 1, 2, 3
 
 request = {0: "VOTE_REQ", 1: "VOTE_POS", 2: "VOTE_NEG", 3: "HEARTBEAT"}
 
-class Server():
+
+class Server:
 
     """docstring for Server."""
 
@@ -32,34 +33,45 @@ class Server():
             req = comm.irecv(source=nb_servers + i)
             self.data[i] = req.wait()
 
+    def save_term(self):
+        filename = "disk/" + str(self.rank) + ".term"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "w") as file:
+            file.write(str(self.term) + '\n')
+
+    def update_term(self, new_term):
+        """ change value of the term and saves it automatically"""
+        self.term = new_term
+        self.save_term()
 
     def respond(self):
         comm.isend(self.data[-1], dest=self.data[-1])
         return self.data.pop()
 
-
-    def handleLog(self, log):
+    def handle_log(self, log):
         for i in range(len(self.log), len(log)):
             self.log.append(log[i])
             self.data.remove(log[i])
 
-
-    def handleRecv(self):
+    def handle_recv(self):
 
         status = MPI.Status()
         is_message = comm.Iprobe(status=status)
 
-        if (not is_message):
+        if not is_message:
             return
 
-        src = status.source #status.Get_source()
+        src = status.source  # status.Get_source()
         tag = status.tag
         term = comm.irecv().wait()
-        print("server number " + str(self.rank) + " source : " + str(src) + " tag : " + request[tag] + " term : " + str(term))
+        print("server number " + str(self.rank)
+              + " source : " + str(src)
+              + " tag : " + request[tag]
+              + " term : " + str(term))
 
-        if (tag == VOTE_REQ):
+        if tag == VOTE_REQ:
             if term > self.term:
-                self.term = term
+                self.update_term(term)
                 self.vote = [-1] * nb_servers
                 self.vote[self.rank] = src
                 self.role = "FOLLOWER"
@@ -68,31 +80,29 @@ class Server():
             else:
                 comm.isend(self.term, dest=src, tag=VOTE_NEG)
 
-        if (tag == VOTE_POS):
+        if tag == VOTE_POS:
             self.vote[src] = self.rank
             nb_vote = len([vote for vote in self.vote if vote == self.rank])
             if nb_vote >= majority:
-                if (self.role != "LEADER"):
+                if self.role != "LEADER":
                     print("server number " + str(self.rank) + " is now leader")
                 self.role = "LEADER"
                 self.leader_heartbeat = time.time()
 
-        if (tag == VOTE_NEG):
+        if tag == VOTE_NEG:
             self.vote[src] = -2
 
-        if (tag == HEARTBEAT):
+        if tag == HEARTBEAT:
             self.role = "FOLLOWER"
             self.timeout += 2
             term, log = term
-            self.term = term
-            self.handleLog(log)
+            self.update_term(term)
+            self.handle_log(log)
 
-
-    def handleSend(self):
-
+    def handle_send(self):
         tmp = time.time()
         if self.role == "CANDIDATE":
-            if (tmp > self.request_vote + 1):
+            if tmp > self.request_vote + 1:
                 print("server number " + str(self.rank) + " is sending VOTE_REQ to everyone")
                 self.request_vote = time.time()
                 for i in range(nb_servers):
@@ -103,35 +113,34 @@ class Server():
             if len(self.data) > 0:
                 value = self.respond()
                 self.log.append(value)
-            if (tmp > self.leader_heartbeat + 1):
+            if tmp > self.leader_heartbeat + 1:
                 self.leader_heartbeat = time.time()
                 for i in range(nb_servers):
                     if i != self.rank:
                         comm.isend((self.term, self.log), dest=i, tag=HEARTBEAT)
 
-
     def consensus(self):
-        #follower
-        #temps aléatoire -> candidat
+        # follower
+        # temps aléatoire -> candidat
         """
         vote pour lui
         votez pour moi
         infini et si leader il reset son timeout jusqu'à avoir plus de la moitié
         """
         terminate = False
-        self.term += 1
+        self.update_term(self.term + 1)
 
         current_time = time.time()
-        self.timeout = current_time + random.randint(3,5)
+        self.timeout = current_time + random.randint(3, 5)
 
         while (current_time <= self.timeout) and not terminate:
             if self.role != "LEADER":
                 current_time = time.time()
-            self.handleRecv()
-            self.handleSend()
+            self.handle_recv()
+            self.handle_send()
             if len(self.data) == 0:
                 time.sleep(1)
-                self.handleSend()
+                self.handle_send()
                 terminate = True
 
         # Too long
@@ -151,7 +160,7 @@ class Server():
         print("server number " + str(self.rank) + " log : " + str(self.log))
 
 
-class Client():
+class Client:
     """docstring for Client."""
 
     def __init__(self, rank):
@@ -163,18 +172,17 @@ class Client():
         for i in range(nb_servers):
             req = comm.isend(self.rank, dest=i)
 
+
 def main():
     rank = comm.Get_rank()
 
-    if (rank < nb_servers):
-        myServer = Server(rank)
-        myServer.run()
+    if rank < nb_servers:
+        my_server = Server(rank)
+        my_server.run()
 
-    if (rank >= nb_servers and rank < (nb_clients + nb_servers)):
-        myClient = Client(rank)
-        myClient.run()
-
-
+    if nb_servers <= rank < (nb_clients + nb_servers):
+        my_client = Client(rank)
+        my_client.run()
 
 
 if __name__ == "__main__":
