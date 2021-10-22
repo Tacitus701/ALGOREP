c0f6f52e893b0aa9374d9c3ae5a0c550b9b1ab1f
ajix <alexandre.pedrosa@epita.fr>
2021-10-22 16:10:39 +0200

    [log] fix log replication and client notification

--- consensus.py
+++ consensus.py
@@ -27,11 +27,10 @@ class Server:
         self.leader_heartbeat = 0
         self.request_vote = 0
         self.log = []
+        self.replicated = []
         self.vote = [-1] * nb_servers
-        self.data = [None] * nb_clients
-        for i in range(nb_clients):
-            req = comm.irecv(source=nb_servers + i)
-            self.data[i] = req.wait()
+        self.waiting_clients = []
+        self.terminate = False
 
     def save_term(self):
         filename = "disk/" + str(self.rank) + ".term"
@@ -44,14 +43,14 @@ class Server:
         self.term = new_term
         self.save_term()
 
-    def respond(self):
-        comm.isend(self.data[-1], dest=self.data[-1])
-        return self.data.pop()
+    def notify_client(self):
+        print("notifying client " + str(self.waiting_clients[0]))
+        comm.isend(self.waiting_clients[0], dest=self.waiting_clients[0])
+        self.waiting_clients.pop(0)
 
     def handle_log(self, log):
         for i in range(len(self.log), len(log)):
             self.log.append(log[i])
-            self.data.remove(log[i])
 
     def process_vote_request(self, src, term):
         if term > self.term:
@@ -75,14 +74,26 @@ class Server:
             self.role = "LEADER"
             self.leader_heartbeat = time.time()
 
-    def process_heartbeat(self, msg):
+    def process_heartbeat(self, src, msg):
         self.role = "FOLLOWER"
         self.timeout += 2
         term, log = msg
         self.update_term(term)
         self.handle_log(log)
+        comm.isend(self.log, dest=src, tag=HEARTBEAT)
 
-    def handle_recv(self):
+    def process_heartbeat_response(self, src, msg):
+        for i in range(len(msg)):
+            self.replicated[i] += 1
+        return
+
+    def process_client_command(self, src, msg):
+        if self.role == "LEADER":
+            self.log.append(msg)
+            self.replicated.append(0)
+            self.waiting_clients.append(src)
+
+    def handle_message(self):
         status = MPI.Status()
         is_message = comm.Iprobe(status=status)
 
@@ -107,7 +118,13 @@ class Server:
             self.vote[src] = -2
 
         if tag == HEARTBEAT:
-            self.process_heartbeat(msg)
+            if self.role != "LEADER":
+                self.process_heartbeat(src, msg)
+            else: # on utilise le tag heartbeat pour repondre au heartbeat
+                self.process_heartbeat_response(src, msg)
+
+        if tag == CLIENT_COMMAND:
+            self.process_client_command(src, msg)
 
     def handle_send(self):
         tmp = time.time()
@@ -120,10 +137,12 @@ class Server:
                         req = comm.isend(self.term, dest=i, tag=VOTE_REQ)
 
         if self.role == "LEADER":
-            if len(self.data) > 0:
-                value = self.respond()
-                self.log.append(value)
+            if len(self.waiting_clients) > 0:
+                # on notifie le client si le log a ete replique chez une majorite
+                if self.replicated[-len(self.waiting_clients)] >= majority:
+                    self.notify_client()
             if tmp > self.leader_heartbeat + 1:
+                self.replicated = [1] * len(self.log)
                 self.leader_heartbeat = time.time()
                 for i in range(nb_servers):
                     if i != self.rank:
@@ -137,36 +156,27 @@ class Server:
         votez pour moi
         infini et si leader il reset son timeout jusqu'à avoir plus de la moitié
         """
-        terminate = False
         self.update_term(self.term + 1)
 
         current_time = time.time()
         self.timeout = current_time + random.uniform(3.0, 5.0)
 
-        while (current_time <= self.timeout) and not terminate:
+        while current_time <= self.timeout:
             if self.role != "LEADER":
                 current_time = time.time()
-            self.handle_recv()
+            self.handle_message()
             self.handle_send()
-            if len(self.data) == 0:
-                time.sleep(1)
-                self.handle_send()
-                terminate = True
 
         # Too long
-        if not terminate:
-            print("server number " + str(self.rank) + " is now candidate")
+        print("server number " + str(self.rank) + " is now candidate")
         self.role = "CANDIDATE"
 
         # Vote for himself
         self.vote[self.rank] = self.rank
 
-        return terminate
-
     def run(self):
-        terminate = False
-        while not terminate:
-            terminate = self.consensus()
+        while not self.terminate:
+            self.consensus()
         print("server number " + str(self.rank) + " log : " + str(self.log))
 
 
@@ -179,8 +189,12 @@ class Client:
 
     def run(self):
         print(str(self.rank) + " : I'm a client")
-        for i in range(nb_servers):
-            req = comm.isend(self.rank, dest=i)
+        nb_req = 1
+        while nb_req > 0:
+            nb_req -= 1
+            time.sleep(random.uniform(5, 8))
+            for i in range(nb_servers):
+                req = comm.isend(self.rank, dest=i, tag=CLIENT_COMMAND)
 
 
 def main():
